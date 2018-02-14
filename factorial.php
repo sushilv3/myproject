04abde8204e802d7650d5cf5152a9ce43fbfe4b3
sushilv3rma@gmail.com <sushil.kumar.verma122@gmail.com>
2018-02-14 19:49:45 +0530

    factorial program

---
+++ factorial.php
@@ -0,0 +1,13 @@
+<?php
+
+function factorial($num)
+{
+    $temp = 1;
+    for ($i = 1; $i <= $num; ++$i) {
+        $temp = $i * $temp;
+    }
+
+    return $temp;
+}
+$fac = factorial(6);
+echo $fac."\n";
